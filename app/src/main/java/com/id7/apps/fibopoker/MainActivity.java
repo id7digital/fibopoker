a34a1c8256e73acabdba91b39177223401581bba
Darren Hyatt <hyatt.darren00@gmail.com>
2019-10-11 09:44:06 +0100

    update

--- app/src/main/java/com/id7/apps/fibopoker/MainActivity.java
+++ app/src/main/java/com/id7/apps/fibopoker/MainActivity.java
@@ -74,56 +74,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cardinfinity.setOnClickListener(this);
         cardquestion.setOnClickListener(this);
         cardcoffee.setOnClickListener(this);
-//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-//                progress = (Math.round(progress/stepSize))*stepSize;
-//                seekBar.setProgress(progress);
-//
-//
-//                switch (progress) {
-//
-//                    case 0:
-//                        image.setImageResource(R.mipmap.ic_foreground_0);
-//                        break;
-//                    case 5:
-//                        image.setImageResource(R.mipmap.ic_foreground_1);
-//                        break;
-//                    case 10:
-//                        image.setImageResource(R.mipmap.ic_foreground_2);
-//                        break;
-//                    case 15:
-//                        image.setImageResource(R.mipmap.ic_foreground_3);
-//                        break;
-//                    case 20:
-//                        image.setImageResource(R.mipmap.ic_foreground_5);
-//                        break;
-//                    case 25:
-//                        image.setImageResource(R.mipmap.ic_foreground_8);
-//                        break;
-//                    case 30:
-//                        image.setImageResource(R.mipmap.ic_foreground_13);
-//                        break;
-//                    case 35:
-//                        image.setImageResource(R.mipmap.ic_foreground_21);
-//                        break;
-//                    case 40:
-//                        image.setImageResource(R.mipmap.ic_foreground_question);
-//                        break;
-//                }
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) {
-//
-//            }
-//        });
-
 
     }
 

fc288bf0a60b0969bf41111959551232554c594f
Darren Hyatt <hyatt.darren00@gmail.com>
2019-10-13 15:09:14 +0100

    update

--- app/src/main/java/com/id7/apps/fibopoker/card.java
+++ app/src/main/java/com/id7/apps/fibopoker/card.java
@@ -1,29 +1,60 @@
 package com.id7.apps.fibopoker;
 
-import android.app.Instrumentation;
-import android.content.Intent;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
+
 
 public class card extends AppCompatActivity {
 
     private TextView card;
+    private String cardNumber;
+
+    // The following are used for the shake detection
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
         card = (TextView) findViewById(R.id.cardShown);
-        String dd = getIntent().getStringExtra("cardNumber");
-        card.setText(dd);
+        cardNumber = getIntent().getStringExtra("cardNumber");
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                 card.setText(cardNumber);
+
+            }
+        });
 
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
+
 
 }
